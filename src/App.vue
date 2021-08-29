1fe8b909fae48596d1be3f68ac694f52b7858eff
unknown <mateusz.figon97@gmail.com>
2021-08-29 22:16:25 +0200

    sass added

--- src/App.vue
+++ src/App.vue
@@ -49,58 +49,6 @@ export default {
   }
 };
 </script>
-<style>
-body {
-  margin: 0;
-  width: 100vw;
-  height: 100vh;
-  overflow: hidden;
-}
-#app {
-  font-family: "Roboto", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  background-color: rgb(247, 247, 247);
-  margin: 0;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-.menu {
-  display: flex;
-  margin: 0 1rem;
-}
-.menu h1 {
-  font-size: 1rem;
-  color: rgb(29, 29, 131);
-  padding: 0 0.1rem;
-}
-#logout {
-  text-transform: uppercase;
-  color: orange;
-  font-weight: 500;
-}
-#logout:hover {
-  cursor: pointer;
-}
-#nav {
-  padding: 4rem;
-  display: flex;
-  align-items: center;
-}
+<style src="./style/style.css">
 
-#nav a {
-  font-weight: 500;
-  color: rgb(29, 29, 131);
-  font-size: 1rem;
-  padding: 0 0.1rem;
-  text-decoration: none;
-}
-
-#nav a.router-link-exact-active {
-  color: orange;
-}
 </style>
